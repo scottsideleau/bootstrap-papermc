aab76e7890f3e4b71745a77c6f54a86d62e8ae3d
Scott R. Sideleau <scott@sideleau.com>
2025-07-13 14:57:12 -0400

    Added Velocity/Paper start-up script

---
+++ papermc.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+QUIET='&> /dev/null'
+JAVA='/usr/bin/java'
+FLAGS='-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:UseAVX=3 -XX:+UseStringDeduplication -XX:+UseFastUnorderedTimeStamps -XX:+UseAES -XX:+UseAESIntrinsics -XX:UseSSE=4 -XX:+UseFMA -XX:AllocatePrefetchStyle=1 -XX:+UseLoopPredicate -XX:+RangeCheckElimination -XX:+EliminateLocks -XX:+DoEscapeAnalysis -XX:+UseCodeCacheFlushing -XX:+SegmentedCodeCache -XX:+UseFastJNIAccessors -XX:+OptimizeStringConcat -XX:+UseCompressedOops -XX:+UseThreadPriorities -XX:+OmitStackTraceInFastThrow -XX:+TrustFinalNonStaticFields -XX:ThreadPriorityPolicy=1 -XX:+UseInlineCaches -XX:+RewriteBytecodes -XX:+RewriteFrequentPairs -XX:+UseNUMA -XX:-DontCompileHugeMethods -XX:+UseFPUForSpilling -XX:+UseFastStosb -XX:+UseNewLongLShift -XX:+UseVectorCmov -XX:+UseXMMForArrayCopy -XX:+UseXmmI2D -XX:+UseXmmI2F -XX:+UseXmmLoadAndClearUpper -XX:+UseXmmRegToRegMoveAll -Dfile.encoding=UTF-8 -Xlog:async -Djava.security.egd=file:/dev/urandom --add-modules jdk.incubator.vector'
+
+# Optimization flags from:
+#    https://github.com/etil2jz/etil-minecraft-flags
+
+# launch Velocity Proxy
+eval pushd /opt/papermc/velocity "$QUIET"
+eval "$JAVA" -Xms2G -Xmx2G "$FLAGS" -jar velocity.jar "$QUIET" &
+eval popd "$QUIET"
+
+# launch PaperMC Server
+eval pushd /opt/papermc "$QUIET"
+eval "$JAVA" -Xms4G -Xmx4G "$FLAGS" -jar paper.jar nogui
+eval popd "$QUIET"
+
